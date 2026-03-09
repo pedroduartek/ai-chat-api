aa29bce89d0df6589c592c9788ef435f51fe2bbd
Pedro Duarte <pedroduartek@gmail.com>
2026-03-09 13:07:54 +0000

    remove unused configs

--- src/Api/Services/WarmupOptions.cs
+++ src/Api/Services/WarmupOptions.cs
@@ -4,6 +4,4 @@ public class WarmupOptions
 {
     public bool Enabled { get; set; } = true;
     public int IntervalMinutes { get; set; } = 2;
-    public string Prompt { get; set; } = "ping";
-    public int MaxTokens { get; set; } = 8;
 }
